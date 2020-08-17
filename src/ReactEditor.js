c33be668f77fbd3ceffa5560a478cd0116d6c0e1
stanimir <s.marinov@structmedia.com>
2020-08-17 18:52:37 +0300

    fixes

--- src/ReactEditor.js
+++ src/ReactEditor.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Simple React component
-import CodemirrorEditor from './CodemirrorEditor';
+import CodemirrorEditor from './CodemirrorEditor.js';
 // import Counter from "./Counter";
 
 // This function creates update callback, which uses react-dom
