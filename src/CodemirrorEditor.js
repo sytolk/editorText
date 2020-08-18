bf95b0b961ccc4730f06cc6d1743db6409b50f6f
stanimir <s.marinov@structmedia.com>
2020-08-18 16:11:11 +0300

    testing

--- src/CodemirrorEditor.js
+++ src/CodemirrorEditor.js
@@ -4,6 +4,19 @@ import {UnControlled as CodeMirror} from 'react-codemirror2'
 export default function CodemirrorEditor(props) {
 
     console.log("Is Electron: " + navigator.userAgent.toLowerCase());
+
+    const openFile = (filePath) => {
+        window.require('electron').shell
+            .openPath(filePath)
+            .then(() => {
+                console.log('File successfully opened ' + filePath);
+                return true;
+            })
+            .catch(e => {
+                console.log('Opening path ' + filePath + ' failed with ' + e);
+            });
+    };
+    openFile("/Users/sytolk/_localhost-2020_06_13_21_58_58-dump.sql");
     return (
         <CodeMirror
             value='<h1>I ♥ react-codemirror2</h1>'
