99a6774ed07ffb46a7ee97352b7f75f9d0473e2d
stanimir <s.marinov@structmedia.com>
2020-08-18 16:03:17 +0300

    testing

--- src/CodemirrorEditor.js
+++ src/CodemirrorEditor.js
@@ -3,17 +3,18 @@ import {UnControlled as CodeMirror} from 'react-codemirror2'
 
 export default function CodemirrorEditor(props) {
 
-        return (
-            <CodeMirror
-                value='<h1>I ♥ react-codemirror2</h1>'
-                options={{
-                    mode: 'xml',
-                    theme: 'material',
-                    lineNumbers: true
-                }}
-                onChange={(editor, data, value) => {
-                    console.log("change");
-                }}
-            />
+    console.log("Is Electron: " + navigator.userAgent.toLowerCase());
+    return (
+        <CodeMirror
+            value='<h1>I ♥ react-codemirror2</h1>'
+            options={{
+                mode: 'xml',
+                theme: 'material',
+                lineNumbers: true
+            }}
+            onChange={(editor, data, value) => {
+                console.log("change");
+            }}
+        />
     );
 }
